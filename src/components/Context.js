48446047150b53e09c9ff4e64be373a6f119b941
leforyou <1147724499@qq.com>
2018-11-30 10:26:46 +0800

    React Context API 的使用

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ThemeContext from '../themeContext';
+import ThemeBar from './ThemeBar';
 
 
 //context是React V16.3版本才有的，它是方便同级的兄弟或父父级组件的之间传递参数
@@ -18,15 +20,31 @@ const themes = {
 export default class Context extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            theme:'light'
+        }
     }
     render() {
         return (
-        <div className="">
-            <div className="text-center">
-                <a href="javascript:" className="btn btn-default">浅色主题</a>
-                <a href="javascript:" className="btn btn-primary">深色主题</a>
-            </div>
-        </div>
+            <ThemeContext.Provider value={themes[this.state.theme]}>
+                <div className="text-center">
+                    <p className="text-center">React Context API的使用</p>
+                    <div className="text-center btn-group">
+                        <button type="button" className="btn btn-default" onClick={this.changeTheme.bind(this,'light')}>浅色主题</button>
+                        <button type="button" className="btn btn-primary" onClick={this.changeTheme.bind(this,'dark')}>深色主题</button>
+                    </div>
+                    <ThemeBar/>
+                </div>
+            </ThemeContext.Provider>
         )
+    }
+    changeTheme(theme){
+        /*console.log(theme);
+        this.setState({
+            theme:theme
+        });*/
+        this.setState({
+            theme
+        });
     }
 }
---
+++ src/components/ThemeBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import ThemeContext from '../themeContext'
+
+const ThemeBar = ()=>{
+    return(
+        <ThemeContext.Consumer>
+            {
+                (theme) => {
+                    //console.log(theme);
+                    return(
+                        <div className="alert" style={{"backgroundColor":theme.bgColor,"color":theme.color}}>
+                            样式区域
+                            <br/>
+                            <button className={theme.className}>样式按钮</button>
+                        </div>
+                    )
+                }
+            }
+        </ThemeContext.Consumer>
+    )
+}
+export default ThemeBar;
+
+/*
+export default class ThemeBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+*/
